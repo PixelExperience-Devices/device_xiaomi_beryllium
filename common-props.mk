6b0d784a879a6697ca1f2f61beac4a5c3ecdac68
rago <rago@google.com>
2016-07-01 16:05:00 -0700

    sdm845-common: Set voice call volume steps to 7
    Bug: 28426597
    Change-Id: Icf78d9acec75c500982b1e4391af4d6f3933da37

--- common-props.mk
+++ common-props.mk
@@ -1,7 +1,8 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.deep_buffer.media=true \
-    ro.af.client_heap_size_kbyte=7168
+    ro.af.client_heap_size_kbyte=7168 \
+    ro.config.vc_call_vol_steps=7
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
