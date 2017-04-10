3e9d08f149694c4a59e5302c8f9a5157c75efcc6
Ruthwar Kumar Ambeer <ruthwar@codeaurora.org>
2017-04-10 15:41:38 +0530

    sdm845-common: Add background atel services in whitelist
    telephony services are white-listed who all need
    to freely run in the background. This is temporary
    work-around and some of the services will be
    removed from the list based on the feasibility.
    
    Change-Id: Ice11429b5b78d5786dc701251b507b7dac5745ee

--- sdm845.mk
+++ sdm845.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
