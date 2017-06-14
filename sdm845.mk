883f4311cf4fd54e11a179266fbe2ad9611ff894
Vinay Verma <vvinay@codeaurora.org>
2017-06-14 14:35:18 +0530

    sdm845-common: Add vndk sp libraries to system lib.
    Change-Id: Ia5b0087506571a929169736dfe108e7ff3f6d40f

--- sdm845.mk
+++ sdm845.mk
@@ -55,3 +55,22 @@ PRODUCT_BOOT_JARS += \
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
+
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hidl.base@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libsync.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    liblzma.vndk-sp
