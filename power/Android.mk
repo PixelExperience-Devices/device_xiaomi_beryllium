69bcde86deaad723773e5c5aae019b212cedd2ca
Rajashekar Adi <radi@codeaurora.org>
2018-04-24 15:02:57 +0530

    sdm845-common: power: Fix for VNDK compliance issue
    VNDK flag enablement for P upgrade
    
    Change-Id: I033c3d9c8a1ee1e7f52a6398ae989becd66d161e

--- power/Android.mk
+++ power/Android.mk
@@ -25,6 +25,8 @@ LOCAL_C_INCLUDES := external/libxml2/include \
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
 
 LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter
 LOCAL_CFLAGS += -DINTERACTION_BOOST
