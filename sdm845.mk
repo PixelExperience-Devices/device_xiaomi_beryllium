4b5a04b101cbde729f90ade4bef59251fa03855f
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    sdm845-common: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e

--- sdm845.mk
+++ sdm845.mk
@@ -51,7 +51,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # IFAA manager
 PRODUCT_PACKAGES += \
