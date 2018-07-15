578fe060505f7150fe37c0e97249bc5a4b224501
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    sdm845-common: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- sdm845.mk
+++ sdm845.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
