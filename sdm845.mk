612c904042d83f3983b88a51fff8bf7845169294
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 23:25:18 -0700

    sdm845-common: Enable vendor and system property isolation
    Change-Id: Ic73cd73c13220b804ec1c0a5fd1bd68470cf9db1

--- sdm845.mk
+++ sdm845.mk
@@ -60,6 +60,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Properties
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
