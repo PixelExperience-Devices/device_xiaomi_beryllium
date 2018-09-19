622aea6627e3d276d3e932de8866aae61b9dec9d
Bruno Martins <bgcngm@gmail.com>
2018-09-19 22:43:08 +0100

    sdm845-common: Import and bind mount stock custom audio policy config
     * The purpose is to apply the same BT A2DP fix that used to be
       applied in the individual device trees.
    
    Change-Id: I500948b5e1e11ee11097ee2e115b0c23dd808a12

--- sdm845.mk
+++ sdm845.mk
@@ -31,6 +31,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc
