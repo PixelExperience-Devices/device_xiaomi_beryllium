daadbfd7207a1d87418e30df807c9b49d7a742f4
Bruno Martins <bgcngm@gmail.com>
2018-09-22 23:43:01 +0100

    sdm845-common: Add framework manifest
     * Mentioning CodeAurora, AtFwd hal is implemented as an Android service
       and it starts from the system partition, that is, it's not a traditional
       vendor hal and hence it should be declared in the framework manifest.
    
    Change-Id: I0aa37ae9b3baacf42dcfa54419f62fc528b00bcb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,7 @@ TARGET_USES_HWC2 := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
