bf2071d293b1bb654d53474fcf227c58fc85a148
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-19 09:54:09 -0300

    Initial bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/aosp_beryllium.mk
--- device.mk
+++ device.mk
@@ -21,8 +21,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
