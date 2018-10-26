d2a146ccfd463125bba430ae605803eb7fd4dda3
Bruno Martins <bgcngm@gmail.com>
2018-10-26 14:11:03 +0100

    sdm845-common: Ship Snap
     * Also allow Snap to use aux camera and remove test camera apps
       from the whitelist while at it.
    
    Change-Id: I65beba444c9ae71b2fe248f201448cacd15e764f

--- common-props.mk
+++ common-props.mk
@@ -12,7 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=true \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.miui.cit,com.qualcomm.qti.qmmi
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # CNE and DPM
 PRODUCT_PROPERTY_OVERRIDES += \
--- sdm845.mk
+++ sdm845.mk
@@ -35,6 +35,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc
