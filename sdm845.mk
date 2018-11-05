a6d189797d60ca31961d659ee4f9457f2dc37609
Jeff Tinker <jtinker@google.com>
2018-11-05 15:04:17 -0800

    sdm845-common: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug:117570956
    bug:110838441
    bug:116252891
    Change-Id: Ie9373543ef416e718a70e75d95bf5b15a2852705

--- sdm845.mk
+++ sdm845.mk
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
