0a34e3ea60cd7db2a890649c42e2c18dacd115aa
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    sdm845-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- sdm845.mk
+++ sdm845.mk
@@ -19,8 +19,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Permissions
 PRODUCT_COPY_FILES += \
