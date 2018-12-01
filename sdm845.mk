4c9818738eb6101b02dd726e3ce0789dd8fa36df
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    sdm845-common: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- sdm845.mk
+++ sdm845.mk
@@ -254,6 +254,12 @@ PRODUCT_PACKAGES += \
     libjson \
     librmnetctl
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
