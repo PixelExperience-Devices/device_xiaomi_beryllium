9c8335aa1f6c843c3250f6f9165150e86ff922d2
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    sdm845-common: Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- sdm845.mk
+++ sdm845.mk
@@ -64,11 +64,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
