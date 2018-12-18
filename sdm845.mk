5cac2501b77ee75f7e1f535c80cac864d028bee9
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    sdm845-common: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- sdm845.mk
+++ sdm845.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
