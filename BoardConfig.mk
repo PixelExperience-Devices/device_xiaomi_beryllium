c0789635829572c9d1e276cc51d2901b3604360e
TheScarastic <warabhishek@gmail.com>
2018-12-31 17:32:06 +0530

    beryllium: Add stock display configs
    Change-Id: I41075b76b14b6647b91e4c9a040ec5d84214e09d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ TARGET_KERNEL_CONFIG := beryllium_defconfig
 # Inherit from the proprietary version
 -include vendor/xiaomi/beryllium/BoardConfigVendor.mk
 
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_beryllium
 TARGET_RECOVERY_DEVICE_MODULES := libinit_beryllium
