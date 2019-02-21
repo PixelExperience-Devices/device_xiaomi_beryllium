b4e837da96baff74258ce6dbeeae684e46bcbb16
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-21 16:01:27 -0300

    Initial changes for caf

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_beryllium
