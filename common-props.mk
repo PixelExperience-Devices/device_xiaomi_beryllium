bccb4ef0a088c68070b11ca062eb15f0f5b8039f
Bruno Martins <bgcngm@gmail.com>
2019-02-24 22:15:12 +0000

    sdm845-common: Set common vendor properties
    Change-Id: I9505e69b9ee2af8c5ba8bb7a97ca6140cddb5100

--- common-props.mk
+++ common-props.mk
@@ -1,13 +1,51 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
+    audio.offload.min.duration.secs=20 \
+    persist.vendor.audio.button_jack.profile=volume \
+    persist.vendor.audio.button_jack.switch=0 \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.ras.enabled=false \
     ro.af.client_heap_size_kbyte=7168 \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.vc_call_vol_steps=11 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    ro.vendor.audio.soundfx.usb=true \
+    vendor.audio.adm.buffering.ms=6 \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.enable.dp.for.voice=false \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio_hal.in_period_size=144 \
+    vendor.audio.hal.output.suspend.supported=false \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio_hal.period_multiplier=3 \
+    vendor.audio.hw.aac.encoder=false \
+    vendor.audio.noisy.broadcast.delay=600 \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=true \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.pstimeout.secs=3 \
+    vendor.audio.offload.track.enable=false \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.voice.path.for.pcm.voip=false \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.bluetooth.soc=cherokee
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.bt.enable.splita2dp=true \
+    vendor.bluetooth.soc=cherokee \
+    vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -21,6 +59,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpm.loglevel=0 \
     persist.vendor.dpm.nsrm.bkg.evt=3955
 
+# Dalvik heap
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.df.dev_name=rmnet_usb0 \
@@ -28,20 +74,61 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.data.mode=concurrent \
     ro.vendor.use_data_netmgrd=true
 
+# Display features
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.df.extcolor.proc=0 \
+    persist.vendor.max.brightness=0
+
 # Display post-processing
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.cabl=0 \
     ro.vendor.display.ad=1 \
     ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
+    ro.vendor.display.cabl=2 \
     ro.vendor.display.sensortype=2
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=0 \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.hw=0 \
+    persist.demo.hdmirotationlock=false \
+    persist.sys.sf.native_mode=0 \
+    ro.opengles.version=196610 \
     sdm.debug.disable_inline_rotator=1 \
     sdm.debug.disable_inline_rotator_secure=1
+
+# Listen
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.soundtrigger=sva \
+    ro.audio.soundtrigger.lowpower=true \
+    ro.vendor.audio.soundtrigger=sva \
+    ro.vendor.audio.soundtrigger.lowpower=true \
+    ro.vendor.audio.soundtrigger.20.key.level=40 \
+    ro.vendor.audio.soundtrigger.20.user.level=60 \
+    ro.vendor.audio.soundtrigger.20.key.adsp.level=40 \
+    ro.vendor.audio.soundtrigger.20.user.adsp.level=40 \
+    ro.vendor.audio.soundtrigger.gmm.level=50 \
+    ro.vendor.audio.soundtrigger.gmm.user.level=10 \
+    ro.vendor.audio.soundtrigger.cnn.level=27 \
+    ro.vendor.audio.soundtrigger.vop.level=10 \
+    ro.vendor.audio.soundtrigger.gmm.adsp.level=50 \
+    ro.vendor.audio.soundtrigger.gmm.user.adsp.level=10 \
+    ro.vendor.audio.soundtrigger.cnn.adsp.level=27 \
+    ro.vendor.audio.soundtrigger.vop.adsp.level=10 \
+    ro.vendor.audio.soundtrigger.training.level=60 \
+    ro.vendor.audio.soundtrigger.hist.duration=1500
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -58,8 +145,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
     vendor.iop.enable_prefetch_ofr=0 \
     vendor.iop.enable_uxe=0
 
@@ -71,12 +160,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
-    persist.vendor.radio.atfwd.start=true \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.flexmap_type=none \
     persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.redir_party_num=1 \
     persist.vendor.radio.report_codec=1 \
+    persist.vendor.radio.sib16_support=1 \
     ril.subscription.types=NV,RUIM \
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    ro.com.android.dataroaming=true \
     ro.telephony.default_network=22,22 \
     telephony.lteOnCdmaDevice=1
 
