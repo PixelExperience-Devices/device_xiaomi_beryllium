66cf41247bae0b53d21af3ebec1c00448ac0912e
Bruno Martins <bgcngm@gmail.com>
2019-03-14 11:59:53 +0100

    sdm845-common: Set up Bluetooth
    Change-Id: I2057e44919354d4cdf172dfbc85da432b32419f0

--- sdm845.mk
+++ sdm845.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbthost_if
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
