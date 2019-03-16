11ee9a0376a531bf0d5aca1f45ea5356e9644274
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-16 17:36:05 -0300

    Revert "beryllium: Implement libinit"
    This reverts commit 3b48880e82e2ffde26f01a828c617bbbf08b23f0.

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -19,7 +19,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco F1
 PRODUCT_MANUFACTURER := Xiaomi
 
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:8.1.0/OPM1.171019.011/V9.6.18.0.OEJMIFD:user/release-keys"
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 8.1.0 OPM1.171019.011 V9.6.18.0.OEJMIFD release-keys" \
     PRODUCT_NAME="beryllium" \
     TARGET_DEVICE="beryllium"
 
