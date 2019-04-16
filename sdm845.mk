477fb02f58acae970b564b153163497078c50e16
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2019-04-16 14:47:16 +0530

    sdm845-common: Switch to composer 2.3
    Change-Id: I9c70051647f81c4adf2431562c2d6b9854a71879

--- sdm845.mk
+++ sdm845.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
