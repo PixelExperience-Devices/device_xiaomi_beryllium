f173238d303d1dae0122de8aecef6e230f2d3c9b
TheScarastic <warabhishek@gmail.com>
2019-05-04 23:10:01 +0530

    sdm845-common: Stop proximity when device is unlocked
    Change-Id: Ia6ddff80c77c4b75fb8dd3b9951d8dcbb0fccbb2

--- pocketmode/src/org/lineageos/pocketmode/PocketModeService.java
+++ pocketmode/src/org/lineageos/pocketmode/PocketModeService.java
@@ -37,8 +37,8 @@ public class PocketModeService extends Service {
         mProximitySensor = new ProximitySensor(this);
 
         IntentFilter screenStateFilter = new IntentFilter();
-        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        screenStateFilter.addAction(Intent.ACTION_USER_PRESENT);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
     }
 
@@ -61,8 +61,8 @@ public class PocketModeService extends Service {
         return null;
     }
 
-    private void onDisplayOn() {
-        if (DEBUG) Log.d(TAG, "Display on");
+    private void onDeviceUnlocked() {
+        if (DEBUG) Log.d(TAG, "Device unlocked");
         mProximitySensor.disable();
     }
 
@@ -74,8 +74,8 @@ public class PocketModeService extends Service {
     private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                onDisplayOn();
+            if (intent.getAction().equals(Intent.ACTION_USER_PRESENT)) {
+                onDeviceUnlocked();
             } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
                 onDisplayOff();
             }
