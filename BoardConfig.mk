98d105db94a6cdcf399b482d62c27f33376c04c5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-06 11:09:05 -0300

    beryllium: Use clang 9.0.5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ BOARD_HAVE_QCOM_FM := true
 # Kernel
 TARGET_KERNEL_CONFIG := beryllium_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/beryllium
-TARGET_KERNEL_CLANG_VERSION := 9.0.2
+TARGET_KERNEL_CLANG_VERSION := 9.0.5
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
