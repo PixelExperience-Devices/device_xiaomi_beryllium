ba4b96e9a1b095a5091efe56a6fc2d79fdef7098
Bruno Martins <bgcngm@gmail.com>
2019-07-30 13:59:40 +0100

    sdm845-common: Compile open source sound FX modules
    Change-Id: Iec50a263c8015dba798c574b4969d3d37b0004bc

--- sdm845.mk
+++ sdm845.mk
@@ -67,6 +67,10 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libvolumelistener \
     tinymix
 
 PRODUCT_COPY_FILES += \
