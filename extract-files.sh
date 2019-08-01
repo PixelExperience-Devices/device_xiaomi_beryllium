3af0a5cad7cd73c141103912a2ce0c5d223ad994
TheScarastic <warabhishek@gmail.com>
2019-08-01 19:37:25 +0530

    beryllium: Nuke hdr at runtime

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Sourced by the common device repo when extracting device-specific blobs
+function blob_fixup() {
+    case "${1}" in
+    vendor/etc/qdcm_calib_data_ebbg_fhd_video_dsi_panel.xml)
+        sed -i "s/\"hdr\"/\"sdr\"/" "${2}"
+        ;;
+    vendor/etc/qdcm_calib_data_tianma_fhd_video_dsi_panel.xml)
+        sed -i "s/\"hdr\"/\"sdr\"/" "${2}"
+        ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
