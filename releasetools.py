79d111f0bf1a1032544c1d10bd6fb1f2351e1c20
Akhil Narang <akhilnarang.1999@gmail.com>
2019-08-05 20:13:02 +0530

    sdm845-common: releasetools: remove unused or unnecessary imports
    Change-Id: Ib6b164182733ccd2ea1d5c885d3746cf14bff397
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import hashlib
-import common
 import re
 
 def FullOTA_InstallEnd(info):
