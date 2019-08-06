1edef9f01572243794d6ae7a4e5ad111cf26a367
Bruno Martins <bgcngm@gmail.com>
2019-08-06 12:06:09 +0100

    sdm845-common: Create power init script
     * Gather all performance related items in one place
       and drop all deprecated services.
    
    Change-Id: I4dbdc164c60136b0957a8c1ff67feafbaf471747

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,6 +25,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
--- sdm845.mk
+++ sdm845.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.rc \
