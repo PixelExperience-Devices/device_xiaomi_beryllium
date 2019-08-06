89de1fc5153d9867a3ded63ff0392efd40105c8f
Bruno Martins <bgcngm@gmail.com>
2019-08-06 15:11:14 +0100

    sdm845-common: Delete class_main init shell script
    Apart from useless battery capacity faking, all this does is to
    start/stop data and radio services based upon the baseband type
    of several QCOM targets. Since all the supported targets in this
    common tree are of the same type, simply start all relevant services
    unconditionally and get rid of one more shell script.
    
    Change-Id: I1297b2537fcbea31d514bab5e248691cabdf523c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -49,14 +49,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.class_main.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.class_main.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
--- sdm845.mk
+++ sdm845.mk
@@ -103,7 +103,6 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
-    init.class_main.sh \
     init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
