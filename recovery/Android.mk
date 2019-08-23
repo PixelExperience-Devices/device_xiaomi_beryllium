cd9634b5212278bfc22e2968800e5e9be707ccc5
argraur <argraur@gmail.com>
2019-08-23 12:17:15 +0100

    sdm845-common: Drop usage of obsolete module tags
    LOCAL_MODULE_TAGS := eng is obsolete. See https://android.googlesource.com/platform/build/+/master/Changes.md#LOCAL_MODULE_TAGS
    
    Change-Id: I3f7d6b60b0885a29891658f903f16a8a77af3484
    Signed-off-by: argraur <argraur@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,5 +7,5 @@ LOCAL_C_INCLUDES := \
     bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_xiaomi
-LOCAL_MODULE_TAGS := eng
+
 include $(BUILD_STATIC_LIBRARY)
