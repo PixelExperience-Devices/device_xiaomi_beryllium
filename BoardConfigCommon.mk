37022184cf83c2da89edc040048b0733f9e8fe84
Bruno Martins <bgcngm@gmail.com>
2019-10-14 21:41:39 +0100

    sdm845-common: Drop no longer needed compat rootfs folders
    Change-Id: Ie5ce442be5166a94bc969e5c145927b1283e47ee

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,11 +107,6 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_ROOT_EXTRA_SYMLINKS := \
-    /mnt/vendor/persist:/persist \
-    /vendor/bt_firmware:/bt_firmware \
-    /vendor/dsp:/dsp \
-    /vendor/firmware_mnt:/firmware
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
