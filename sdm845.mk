8def897276d8898d55f2da9b9f302c1119ec56c9
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    sdm845-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- sdm845.mk
+++ sdm845.mk
@@ -20,6 +20,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
