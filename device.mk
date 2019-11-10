94ca81bcc08448afc69c016bc3d54ee8639a062f
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-11-10 16:06:54 -0200

    beryllium: Switch to cryptfshw QSEECom impl
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -61,7 +61,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # Input
 PRODUCT_COPY_FILES += \
