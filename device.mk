0e0b89fe1ed2596e056cbb941acd73c1a75dfbe4
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-22 16:14:15 -0300

    beryllium: Include firmware
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
+# Firmware
+$(call inherit-product, vendor/xiaomi/firmware/beryllium/firmware.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2246
 TARGET_SCREEN_WIDTH := 1080
