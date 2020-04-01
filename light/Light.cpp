97ea78031eb41b546dd0b6cc0d23ab6e1caa06b7
LuK1337 <priv.luk@gmail.com>
2020-04-01 11:32:20 +0200

    beryllium: lights: Make notification led priority > battery led one
    Change-Id: Ibece77fb3495f9add70668d8ca0e74a8fe66d83a

--- light/Light.cpp
+++ light/Light.cpp
@@ -64,8 +64,8 @@ static uint32_t getBrightness(const LightState& state) {
 
 Light::Light() {
     mLights.emplace(Type::ATTENTION, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 0));
-    mLights.emplace(Type::BATTERY, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 1));
-    mLights.emplace(Type::NOTIFICATIONS, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 2));
+    mLights.emplace(Type::BATTERY, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 2));
+    mLights.emplace(Type::NOTIFICATIONS, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 1));
 }
 
 void Light::handleWhiteLed(const LightState& state, size_t index) {
