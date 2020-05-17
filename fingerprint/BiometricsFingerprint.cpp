5673a036335a3e659bb5b4111cc165409923108b
Bruno Martins <bgcngm@gmail.com>
2020-05-17 22:13:56 +0100

    sdm845-common: fingerprint: Allow loading FOD modules
    As seen in the vendor image dumps, equuleus ships with goodix_fod
    and ursa with goodix_fod or synaptics.
    
    Change-Id: I2ccef59698a570dcb35c331dd7994fb7f7af68f0

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -272,20 +272,15 @@ fingerprint_device_t* getDeviceForVendor(const char* class_name) {
 
 fingerprint_device_t* getFingerprintDevice() {
     fingerprint_device_t* fp_device;
+    std::string vendor_modules[] = {"fpc", "goodix", "goodix_fod", "syna"};
 
-    fp_device = getDeviceForVendor("fpc");
-    if (fp_device == nullptr) {
-        ALOGE("Failed to load fpc fingerprint module");
-    } else {
-        setFpVendorProp("fpc");
-        return fp_device;
-    }
+    for (const auto& vendor : vendor_modules) {
+        if ((fp_device = getDeviceForVendor(vendor.c_str())) == nullptr) {
+            ALOGE("Failed to load %s fingerprint module", vendor.c_str());
+            continue;
+        }
 
-    fp_device = getDeviceForVendor("goodix");
-    if (fp_device == nullptr) {
-        ALOGE("Failed to load goodix fingerprint module");
-    } else {
-        setFpVendorProp("goodix");
+        setFpVendorProp(vendor.c_str());
         return fp_device;
     }
 
