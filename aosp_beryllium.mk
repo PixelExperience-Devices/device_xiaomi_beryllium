a0e55b1fbea1ee8039db481dbbdfa5e5b0508c90
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-06-07 14:47:57 +0530

    Beryllium: Include Google Recorder
    Signed-off-by: Lakshay Garg <lakshaygarg.in@gmail.com>

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -11,6 +11,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
