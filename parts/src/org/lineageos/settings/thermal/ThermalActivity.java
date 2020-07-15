0a5e64a329c3c3132d9e57ca8b0af0fea4e5dd41
Jyotiraditya <jyotiraditya@aospa.co>
2020-07-15 06:58:11 +0200

    beryllium: parts: Add back button to ThermalProfiles.
    Change-Id: Iacafbde624ef80456d502fe6744125bbb8fbb618
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>
    Signed-off-by: resist15 <souravhjagtap21@gmail.com>

--- parts/src/org/lineageos/settings/thermal/ThermalActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -18,6 +18,7 @@ package org.lineageos.settings.thermal;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
 public class ThermalActivity extends PreferenceActivity {
 
@@ -27,7 +28,18 @@ public class ThermalActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
