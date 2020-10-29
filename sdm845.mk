153284b9697bd5189bb3890c7cae1ea146c8aef4
LuK1337 <priv.luk@gmail.com>
2020-10-29 14:31:02 +0100

    sdm845-common: Enable RRO overlays for lineage-sdk
    Change-Id: I482f2393e10e90362db2c66b7abb0de837085843

--- sdm845.mk
+++ sdm845.mk
@@ -22,7 +22,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay-system
 
 # Permissions
