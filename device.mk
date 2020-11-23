653fd36545ae7deed40744ec267ca2ce92960011
Bruno Martins <bgcngm@gmail.com>
2020-11-23 22:12:56 +0000

    beryllium: Ditch VR HAL
    Change-Id: Id85d2dd3eb1694cbc6c5246f2aeb92462d15f2cf

--- device.mk
+++ device.mk
@@ -358,12 +358,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so
 
-# VR
-PRODUCT_PACKAGES += \
-    android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service \
-    vr.sdm845
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
