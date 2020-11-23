7a6b6e61ede719f90be60c314d54dfe3e8e630ee
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-11-23 16:00:11 +0000

    beryllium: use same kernel as polaris
    Signed-off-by: Lakshay Garg <lakshaygarg.in@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,8 @@ BOARD_HAVE_QCOM_FM := true
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG := beryllium_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/beryllium
+TARGET_KERNEL_CONFIG := beryllium_defconfig
 TARGET_KERNEL_CLANG_VERSION := proton
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 KERNEL_SUPPORTS_LLVM_TOOLS := true
