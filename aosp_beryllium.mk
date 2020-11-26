6975facd1db4273e1e8d87c45c764efe20e9b8b2
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-11-26 11:39:54 +0000

    beryllium: remove redundant flags and overlays
    They are now set to true by default
    Signed-off-by: Lakshay Garg <lakshaygarg.in@gmail.com>

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -10,8 +10,6 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
