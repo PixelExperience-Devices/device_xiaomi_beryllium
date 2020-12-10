0012ed5e992d8e1fbffa407ef12217ac5ff0a978
Bruno Martins <bgcngm@gmail.com>
2020-12-10 15:01:22 +0000

    sdm845-common: Drop bitness from graphics composer service
    Change-Id: I697d27a7d76aa25c979d8566d12a765c39403ae6

--- sdm845.mk
+++ sdm845.mk
@@ -143,7 +143,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service:64 \
+    android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
