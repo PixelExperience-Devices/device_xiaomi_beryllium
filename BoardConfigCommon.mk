1e57d9fbacc3cb2a11718527ce236e1e2b98841c
Bruno Martins <bgcngm@gmail.com>
2020-12-14 14:10:30 +0000

    sdm845-common: Update blobs from MIUI 12
    Change-Id: I72984743e890c4ac9444f3003d8e9a225da7866c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,7 +138,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-05-01
+VENDOR_SECURITY_PATCH := 2020-09-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
