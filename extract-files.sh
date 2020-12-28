e7251da1522581cb4801b24f967bfb056ff2e945
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    beryllium: Align with extraction-utils templates
    Change-Id: Ifbf18f79a1d0f173cb5b83bdf0178db2a5f1373e

--- extract-files.sh
+++ extract-files.sh
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -27,11 +28,8 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=beryllium
 export DEVICE_COMMON=sdm845-common
 export VENDOR=xiaomi
-
-export DEVICE_BRINGUP_YEAR=2018
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
