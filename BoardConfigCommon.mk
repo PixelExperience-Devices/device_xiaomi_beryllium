b43d7d6422550b219691bd64ec2ddb783838c8ad
Bruno Martins <bgcngm@gmail.com>
2021-01-03 19:02:09 +0000

    sdm845-common: Move CNE and DPM properties around
    CNE blobs are shipped in /vendor and DPM blobs have been recently
    moved to /system_ext.
    
    Change-Id: Ide4881bb4bad52f874b70f77d5fd79fd58326862

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,7 @@ TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
