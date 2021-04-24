eb6f35f4f8594cb0c9fde2155158378aefba6e81
Bruno Martins <bgcngm@gmail.com>
2021-04-24 15:48:15 +0100

    beryllium: Build disabled VBMeta image for all targets
    This was never needed for any mi845 target besides perseus
    (all the rest seem to have just empty partitions),
    but should be set here so it fixes signing builds with own keys.
    
    Change-Id: Idded651f3b23535581a555bc2a27409489d8da1b

--- scripts/releasetools.py
+++ scripts/releasetools.py
@@ -45,6 +45,7 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
 
 def AddTrustZoneAssertion(info, input_zip):
