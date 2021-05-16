fd46234e87be82610d1136b9c7a05962d395875d
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-05-16 08:13:13 +0530

    beryllium: Import msm_irqbalance from qcom/common
    * Also move powerhint.json to configs/perf while we're at it.
    
    Change-Id: Ic9b598854fcbc2200293ec05d9405adba1d52930
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
     WifiOverlay
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
