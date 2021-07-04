f0f0ca75c26f00b7e00035b2e07857d13ca5d6a9
Marc Bourgoin <themard85@gmail.com>
2021-07-04 15:02:02 -0600

    beryllium: Switch to AOSP WFD
    Change-Id: I3dd591444dc54ebe3a32e0385cbfc3db6bfc5e1f

--- device.mk
+++ device.mk
@@ -404,11 +404,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
