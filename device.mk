95eae8fbe1f421d6b9f69b0d839461768c07ad8e
Chenyang Zhong <zhongcy95@gmail.com>
2021-08-31 01:45:15 +0000

    beryllium: switch to the in-tree vibrator HAL
    Change-Id: I98e3c95d6771e05f93286e661eda85bc2f4d30c4
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -373,8 +373,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service.beryllium
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_COPY_FILES += \
