714d86606eed6105f9ce9c8267048ce054028dcd
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-09 11:30:34 -0400

    beryllium: use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    BOARD_PLAT_PRIVATE_SEPOLICY_DIR and BOARD_PLAT_PUBLIC_SEPOLICY_DIR
    has been deprecated.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Ieb8e15315e0c5b79a48938e8fdc9a524835ef577

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,8 +153,8 @@ VENDOR_SECURITY_PATCH := 2020-12-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
