f5295ee80445c039b413353dfcaa064e90437b40
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-19 09:37:49 -0400

    beryllium: turn off compressed apex
    Google introduced compressed apex on android 12 [1], and the flag is
    enabled by default in updatable_apex.mk [2]. Polaris cannot boot
    compressed apex with encryption, so disable it.
    
    ref:
    [1] https://source.android.com/devices/tech/ota/apex#compressed-apex
    [2] https://android.googlesource.com/platform/build/+/4a1240de8a2ee5795a30df123058677d3999145e
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Lakshay <lakshaygarg.in@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
