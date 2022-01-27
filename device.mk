da14833d6b463e99d1c701e699bfd8856ca5427b
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    beryllium: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- device.mk
+++ device.mk
@@ -49,7 +49,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Charger
