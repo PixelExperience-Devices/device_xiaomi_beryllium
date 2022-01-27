b5f70c915077d3bff7cbd75c34be468a5236e180
jjpprrrr <zhongcy95@gmail.com>
2022-01-27 22:01:21 +0100

    beryllium: build kernel with Prelude Clang
    Etude finally works with Clang 14 and LTO.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Lakshay <lakshaygarg.in@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_DENSITY := 440
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/beryllium
 TARGET_KERNEL_CONFIG := beryllium_defconfig
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CLANG_VERSION := prelude
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
 KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 # HIDL
