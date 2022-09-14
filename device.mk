bc4caf79c53cf456911022d30e678e050738b238
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    beryllium: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm-service.clearkey \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
