374d653b1b48300968084965c6bccf7a3ba2ab3b
kubersharma001 <kubersharma001@gmail.com>
2022-12-30 12:15:51 +0530

    beryllium: Switch to sepolicy_vndr-legacy-um
    Change-Id: I92dd52a45e5b42f58e18e90d303f0be4e6fd14c4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-12-01
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
